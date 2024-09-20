d8b371c49c634eee2dff26a5df0a6c869dcc016a
Robodlan <azuldevs@zohomail.com>
2024-09-20 10:57:15 -0400

    addMember

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let group = [];
+let member = [];
 
 function addExpense() {
   const groupEl = document.getElementById("group").value;
@@ -13,12 +14,29 @@ function addExpense() {
 function displayExpense() {
   const groupNameExpenses = document.getElementById("group-name-expenses");
   groupNameExpenses.innerHTML = "";
+  const li = document.createElement('li')
   group.forEach(
     (group, index) =>
-      (groupNameExpenses.innerHTML = ` <p>Name: ${group.groupEl} Expense: $${
+      (groupNameExpenses.innerHTML = ` <h1>${group.groupEl} Expense $${
         group.expense
-      } ID: ${index + 1}</p> `)
+      } ID: ${index + 1}</h1> 
+        <h3>Group Members</h3>
+        <ul></ul> 
+      `)
   );
+}
+
+function addMember() {
+  const members = document.getElementById("members").value
+  member.forEach(mem => {
+    if (mem === members) {
+      alert(`${members} is already a member of the group.`);
+      return;
+    } else {
+      member.push(members);
+      displayMembers(members);
+    }
+  })
 }
 
 function clearInputs() {
